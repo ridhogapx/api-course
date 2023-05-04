b16b62830d1f5f69d7fdb4262b3f4c9641e0490f
RageNeko26 <rneko2006@gmail.com>
2023-05-04 22:12:58 +0700

    Update

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 5000;
+const port = 3000;
+const dummyData = {
+    id: 6969,
+    message: 'Data is retrieved',
+    success: true
+};
 app.get('/', (req, res) => {
-    res.send('Test');
+    res.send(JSON.stringify(dummyData));
 });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
 import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
-const port: number = 5000;
+const port: number = 3000;
+
+interface Dummy {
+	id: number,
+	message: string,
+	success: boolean,
+}
+
+const dummyData: Dummy = {
+	id: 6969,
+	message: 'Data is retrieved',
+	success: true
+}
+
 
 app.get('/', (req: Request, res: Response):void => {
-	res.send('Test');
+	res.send(JSON.stringify(dummyData));
 });
 
 app.listen(port, ():void => {
