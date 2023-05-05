d617936b63bc4232d9062860420ba9cfbe0866bf
RageNeko26 <rneko2006@gmail.com>
2023-05-05 15:08:27 +0700

    Todo: Adding Hash for password security

--- dist/index.js
+++ dist/index.js
@@ -4,17 +4,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({ extended: false });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'course_api'
+});
 const app = (0, express_1.default)();
 const port = 3000;
-// Test Dummy data
-const dummyData = {
-    id: 6969,
-    message: 'Data is retrieved',
-    success: true
-};
-app.get('/', (req, res) => {
-    res.send(JSON.stringify(dummyData));
+;
+app.post('/api/user', urlEncodedParser, (req, res) => {
+    const response = {
+        message: `Success for adding user`,
+        success: true,
+        data: [
+            {
+                email: req.body.email,
+                password: req.body.password
+            }
+        ]
+    };
+    res.end(JSON.stringify(response));
 });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+connection.connect();
+connection.end();
--- index.ts
+++ index.ts
@@ -1,28 +1,53 @@
 import express, { Express, Request, Response } from 'express';
+ 
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const urlEncodedParser:any = bodyParser.urlencoded({extended: false});
+
+
+const connection: any = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'course_api'
+});
+
 
 const app: Express = express();
 const port: number = 3000;
 
-interface Dummy {
-	id: number,
+interface APIResponse {
 	message: string,
-	success: boolean,
+	success: boolean
+	data: Data[]
+};
+
+interface Data {
+	email: string,
+	password: string
 }
 
-// Test Dummy data
-const dummyData: Dummy = {
-	id: 6969,
-	message: 'Data is retrieved',
-	success: true
-}
+app.post('/api/user', urlEncodedParser , (req: Request, res: Response):void => {	
+	const response: APIResponse = {
+		message: `Success for adding user`,
+		success: true,
+		data: [
+			{
+				email: req.body.email,
+				password: req.body.password
+			}
+		]
+	}
 
-
-app.get('/', (req: Request, res: Response):void => {
-	res.send(JSON.stringify(dummyData));
+	res.end(JSON.stringify(response));
 });
 
 app.listen(port, ():void => {
 	console.log(`Server is running on port ${port}`);
-})
+});
 
+connection.connect();
+
+connection.end();
 
