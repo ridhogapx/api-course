343dcd75b6b9138f549297a27eacb6bf8cc62c0e
RageNeko26 <rneko2006@gmail.com>
2023-05-24 00:21:52 +0700

    Update: Adding logic for fetch all data course

---
+++ models/Course/AllCourse.ts
@@ -0,0 +1,25 @@
+import { Course } from './Schema';
+import ResponseAPI from '../../interfaces/ResponseAPI';
+
+const getAllCourse = async(req: any, res:any): Promise<any> => {
+	try {
+		const allCourse = await Course.findAll();
+		const successResponse: ResponseAPI = {
+			message: 'Berhasil mendapatkan data materi!',
+			success: true,
+			status: 200,
+			data: [allCourse]
+		}
+
+		res.json(successResponse);
+	} catch(err) {
+		const failResponse: ResponseAPI = {
+			message: 'Data tidak ditemukan!',
+			success: false,
+			status: 404,
+			data: []
+		}
+	}
+}
+
+export default getAllCourse;
