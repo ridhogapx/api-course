d3d8a67ab077cb98c9e9879f9a49c20bb8d98818
RageNeko26 <rneko2006@gmail.com>
2023-05-23 23:48:55 +0700

    Fix: Escaping url

--- models/Course/AddCourse.ts
+++ models/Course/AddCourse.ts
@@ -6,13 +6,15 @@ const { validationResult }:any = require('express-validator');
 const AddCourse = async(req: any, res:any): Promise <any> => {
 	const result: any = validationResult(req);
 
+	// Escaping URL
+	const YT_URL: any = req.body.yt_url;
+	const valid_url: any = YT_URL.replaceAll('&#x2F;', '/');
+
 	const successResponse: ResponseAPI = {
 		message: 'Berhasil menambahkan materi!',
 		success: true,
 		status: 201,
-		data: [{
-			url: decodeURI(req.body.yt_url)
-		}]
+		data: []
 	}
 
 
@@ -22,13 +24,10 @@ const AddCourse = async(req: any, res:any): Promise <any> => {
 		});
 	}
 
-	// Disable dulu gak sih 
-	
-	// await Course.create({
-	// 	title: req.body.title,
-	// 	yt_url: req.body.yt_url
-	// })
-	res.set({'content-type': 'application/json; charset=utf-8'})
+	await Course.create({
+		title: req.body.title,
+		yt_url: valid_url
+	})
 
 	res.json(successResponse);
 
