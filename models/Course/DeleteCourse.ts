55afef2e123e9ba11a2a974004131c6810b988a6
RageNeko26 <rneko2006@gmail.com>
2023-05-24 22:58:00 +0700

    Adding logic for deleting Course

---
+++ models/Course/DeleteCourse.ts
@@ -0,0 +1,34 @@
+import { Course } from './Schema';
+import ResponseAPI from '../../interfaces/ResponseAPI';
+import checkID from '../../middlewares/ValidateDB/ValidateID';
+
+const deleteCourse = async(req: any, res: any): Promise<any> => {
+	const id = req.params.id;
+	const validate = await checkID(id);
+
+	if(!validate) {
+		const notFound: ResponseAPI = {
+			message: `Data tidak ditemukan!`,
+			success: false,
+			status: 404,
+			data: []
+		}
+
+		return res.json(validate);
+	}
+
+	await Course.destroy({
+		where: {
+			id: id
+		}
+	});
+
+	const successResponse: ResponseAPI = {
+		message: 'Berhasil menghapus data!',
+		success: true,
+		status: 201,
+		data: []
+	}
+
+	return res.json(successResponse);
+}
