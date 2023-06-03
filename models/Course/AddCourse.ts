ee4f42a9c6a9338dcc8c92b9356c80ff9d90e4cf
RageNeko26 <rneko2006@gmail.com>
2023-06-03 21:13:22 +0700

    Clean up code

--- models/Course/AddCourse.ts
+++ models/Course/AddCourse.ts
@@ -1,14 +1,14 @@
-import { Course } from './Schema';
-import ResponseAPI from '../../interfaces/ResponseAPI';
+import { Course } from './Schema'
+import ResponseAPI from '../../interfaces/ResponseAPI'
 
-const { validationResult }:any = require('express-validator');
+const { validationResult }:any = require('express-validator')
 
 const AddCourse = async(req: any, res:any): Promise <any> => {
-	const result: any = validationResult(req);
+	const result: any = validationResult(req)
 
 	// Escaping URL
-	const YT_URL: any = req.body.yt_url;
-	const valid_url: any = YT_URL.replaceAll('&#x2F;', '/');
+	const YT_URL: any = req.body.yt_url
+	const valid_url: any = YT_URL.replaceAll('&#x2F;', '/')
 
 	const successResponse: ResponseAPI = {
 		message: 'Berhasil menambahkan materi!',
@@ -21,7 +21,7 @@ const AddCourse = async(req: any, res:any): Promise <any> => {
 	if(!result.isEmpty()) {
 		return res.json({
 			errors: result.array()
-		});
+		})
 	}
 
 	await Course.create({
@@ -29,8 +29,8 @@ const AddCourse = async(req: any, res:any): Promise <any> => {
 		yt_url: valid_url
 	})
 
-	return res.json(successResponse);
+	return res.json(successResponse)
 
 }
 
-export default AddCourse;
+export default AddCourse
