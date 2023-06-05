35d93bdd664e2a279662870e5292048a1ea6c368
RageNeko26 <rneko2006@gmail.com>
2023-06-05 17:17:55 +0700

    fix: redirect to client apps

--- routes/GoogleProtected.ts
+++ routes/GoogleProtected.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from 'express'
 import { GoogleSchema } from '../models/Google/Schema'
-import ResponseAPI from '../interfaces/ResponseAPI'
 import generateToken from '../middlewares/Token/TokenGenerator'
 
-const GoogleProtected = async(req: Request | any, res: Response): Promise<Response | undefined> => {
+const GoogleProtected = async(req: Request | any, res: Response): Promise<Response | void> => {
 	const finder = await GoogleSchema.findAll({
 		where: {
 			email: req.user.email
@@ -15,23 +14,16 @@ const GoogleProtected = async(req: Request | any, res: Response): Promise<Respon
 		const email: string = finder[0].email
 		const token: string = generateToken(email,role)
 
-		const response: ResponseAPI = {
-			message: 'Berhasil login',
-			success: true,
-			status: 201,
-			data: []
-		}
-
-		return res.json(response)
+		return res.redirect('http://localhost:5173/'+ token)
 
 		// return res.redirect('http://localhost:5173/' + token)
 		
-	} else {
-		await GoogleSchema.create({
-			email: req.user.email,
-			name: req.user.displayName
-		})
-	}
+	} 
+	await GoogleSchema.create({
+		email: req.user.email,
+		name: req.user.displayName
+	})
+	
 
 }
 
